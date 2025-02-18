66f7ab35efd7c6a5e9486e0eaa04a8bf4bff20ab
ahmedadhem <ahmedadhem330@gmail.com>
2025-02-18 20:47:41 +0200

    commit first

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/About" element={<About />} />
         <Route path="/Pricing" element={<Pricing />} />
         <Route path="/Courses" element={<Courses />} />
         <Route path="/Contact" element={<Contact />} />
